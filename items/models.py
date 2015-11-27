44a762b484344d95eed03a84711863ab67913ec1
Shipra <code.shipra@gmail.com>
2015-11-28 02:01:06 +0530

    adds vies to bids app

--- items/models.py
+++ items/models.py
@@ -5,8 +5,8 @@ class Items(models.Model):
     Name = models.CharField(max_length=200)
     date_added = models.DateTimeField('date added')
     image = models.ImageField()
-    time_left =   models.DateTimeField('time left')
-
+    #time_left =   models.DateTimeField('time left')
+    #create a time left function
 
 class bids(models.Model):
     item = models.ForeignKey(Items)
--- items/views.py
+++ items/views.py
@@ -1,15 +1,14 @@
 from django.http import HttpResponse
 from datetime import datetime
-from 
+from items.models import Items, bids
 
 
 def add_items(request):
     if request.method == "POST":
-        item
-        name = request.POST['name']
-        date_added = datetime.now()
-
-        return HttpResponse("Added User: {} {}".format(name,email))
+        item= Items.objects.create(name = request.POST['name'],
+                                    date_added = datetime.now(),
+                                    image= request.POST['image_url'])
+        return HttpResponse("Added Item: {} {}".format(item.name))
 
     return HttpResponse("OK 200 Items Added")
 
