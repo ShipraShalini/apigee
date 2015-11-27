0ee15a8d56fa939d07315642afcbd215fcfbd4b0
Shipra <code.shipra@gmail.com>
2015-11-28 02:02:40 +0530

    adds vies to bids app

--- items/models.py
+++ items/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Items(models.Model):
-    Name = models.CharField(max_length=200)
+    Name = models.CharField(max_length=200, primary_key=True)
     date_added = models.DateTimeField('date added')
     image = models.ImageField()
     #time_left =   models.DateTimeField('time left')
--- items/views.py
+++ items/views.py
@@ -8,12 +8,39 @@ def add_items(request):
         item= Items.objects.create(name = request.POST['name'],
                                     date_added = datetime.now(),
                                     image= request.POST['image_url'])
-        return HttpResponse("Added Item: {} {}".format(item.name))
+        return HttpResponse("Added Item: {}".format(item.name))
 
-    return HttpResponse("OK 200 Items Added")
 
 def del_items(request):
-    return HttpResponse("OK 200 Item deleted")
+    item_name=request.REQUEST['name']
+    Items.objects.get(name = item_name).delete()
+    return HttpResponse("OK 200 Item deleted {}".format(item_name))
+
 
 def view_items(request):
-    return HttpResponse("OK 200 Items Listed")
+    item_name=request.REQUEST['name']
+    item = Items.objects.get(name = item_name)
+    #find top 5 bids
+    return HttpResponse("OK 200 Item Listed: {}".format(item))
+
+
+def add_bid(request):
+    if request.method == "POST":
+        bid = bids.objects.create(bidder = request.POST['name'],  #find out way of retrieving user form login info
+                                    item = request.POST['item'],
+                                    bid_amount= request.POST['amount'])
+        return HttpResponse("Added Item: {} {}".format(bid.item, bid.bid_amount))
+
+
+def del_bids(request):
+    item_name=request.REQUEST['name']
+    bidder = request.REQUEST['name']
+    bids.objects.get(Q(name__exact = item_name),
+                     Q(bidder__exact = bidder  )).delete()
+    return HttpResponse("OK 200 Bid deleted {} for ".format(item_name))
+
+
+def view_bids(request):
+    bidder=request.REQUEST['name']
+    bid_list = bids.objects.filter(name = bidder)
+    return HttpResponse("OK 200 \nBids Listed: {}".format(bid_list))
