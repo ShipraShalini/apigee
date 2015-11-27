fe7fa699d5fcde092cfdd98a98e389bfaf658a49
Shipra <code.shipra@gmail.com>
2015-11-28 02:34:04 +0530

    adds status 200 to HttpResponse

--- items/views.py
+++ items/views.py
@@ -8,20 +8,20 @@ def add_items(request):
         item= Items.objects.create(name = request.POST['name'],
                                     date_added = datetime.now(),
                                     image= request.POST['image_url'])
-        return HttpResponse("Added Item: {}".format(item.name))
+        return HttpResponse("Added Item: {}".format(item.name), status= 200)
 
 
 def del_items(request):
     item_name=request.REQUEST['name']
     Items.objects.get(name = item_name).delete()
-    return HttpResponse("OK 200 Item deleted {}".format(item_name))
+    return HttpResponse("OK 200 Item deleted {}".format(item_name), status= 200)
 
 
 def view_items(request):
     item_name=request.REQUEST['name']
     item = Items.objects.get(name = item_name)
     #find top 5 bids
-    return HttpResponse("OK 200 Item Listed: {}".format(item))
+    return HttpResponse("OK 200 Item Listed: {}".format(item), status= 200)
 
 
 def add_bid(request):
@@ -29,7 +29,9 @@ def add_bid(request):
         bid = bids.objects.create(bidder = request.POST['name'],  #find out way of retrieving user form login info
                                     item = request.POST['item'],
                                     bid_amount= request.POST['amount'])
-        return HttpResponse("Added Item: {} {}".format(bid.item, bid.bid_amount))
+        #function for notifying bidders
+
+        return HttpResponse("Added Item: {} {}".format(bid.item, bid.bid_amount), status= 200)
 
 
 def del_bids(request):
@@ -37,10 +39,10 @@ def del_bids(request):
     bidder = request.REQUEST['name']
     bids.objects.get(Q(name__exact = item_name),
                      Q(bidder__exact = bidder  )).delete()
-    return HttpResponse("OK 200 Bid deleted {} for ".format(item_name))
+    return HttpResponse("OK 200 Bid deleted {} for ".format(item_name), status= 200)
 
 
 def view_bids(request):
     bidder=request.REQUEST['name']
     bid_list = bids.objects.filter(name = bidder)
-    return HttpResponse("OK 200 \nBids Listed: {}".format(bid_list))
+    return HttpResponse("OK 200 \nBids Listed: {}".format(bid_list), status= 200)
